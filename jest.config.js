91e29bc539e1761bf7340dbcca89f3a2ef59ed85
Nguyen Van Giang <giang16012405@gmail.com>
2025-10-12 22:24:41 +0700

    chore(jest): mock static assets (images/svg) via moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,7 @@ export default {
   transform: { '^.+\\.[jt]sx?$': 'babel-jest' },
   moduleNameMapper: {
     '\\.(css|scss|sass|less)$': 'identity-obj-proxy',
+    '\\.(png|jpe?g|gif|bmp|webp|avif)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.svg$': '<rootDir>/__mocks__/svgrMock.js'
   },
 };
