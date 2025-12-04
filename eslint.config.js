66927a0e7ead91ef1b94be00352fbe84ac197d82
Nguyen Van Giang <giang16012405@gmail.com>
2025-12-05 01:08:27 +0700

    chore(test, ui): configure Storybook for UI testing

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js'
 import importPlugin from 'eslint-plugin-import'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
@@ -5,53 +8,50 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 
-export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'jsx-a11y': jsxA11y,
-      'import': importPlugin,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      ...jsxA11y.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-
-    /* Sắp xếp import ổn định, nhóm & chèn dòng trống giữa nhóm */
-    'import/order': ['error', {
-      'groups': [
-        'builtin', 
-        'external',
-        'internal',
-        ['parent', 'sibling', 'index'],
-        'object', 'type'
-      ],
-      'newlines-between': 'always',
-      'alphabetize': { order: 'asc', caseInsensitive: true }
-    }],
-
-    /* Một vài luật a11y tối thiểu cho form/button/label/link */
-    'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }],
-    'jsx-a11y/anchor-is-valid': 'warn',
-    'jsx-a11y/interactive-supports-focus': 'warn',
-    'jsx-a11y/no-autofocus': 'warn',
+export default [{ ignores: ['dist'] }, {
+  files: ['**/*.{js,jsx}'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+    parserOptions: {
+      ecmaVersion: 'latest',
+      ecmaFeatures: { jsx: true },
+      sourceType: 'module',
     },
   },
-]
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+    'jsx-a11y': jsxA11y,
+    'import': importPlugin,
+  },
+  rules: {
+    ...js.configs.recommended.rules,
+    ...reactHooks.configs.recommended.rules,
+    ...jsxA11y.configs.recommended.rules,
+    'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+
+  /* Sắp xếp import ổn định, nhóm & chèn dòng trống giữa nhóm */
+  'import/order': ['error', {
+    'groups': [
+      'builtin', 
+      'external',
+      'internal',
+      ['parent', 'sibling', 'index'],
+      'object', 'type'
+    ],
+    'newlines-between': 'always',
+    'alphabetize': { order: 'asc', caseInsensitive: true }
+  }],
+
+  /* Một vài luật a11y tối thiểu cho form/button/label/link */
+  'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }],
+  'jsx-a11y/anchor-is-valid': 'warn',
+  'jsx-a11y/interactive-supports-focus': 'warn',
+  'jsx-a11y/no-autofocus': 'warn',
+  },
+}, ...storybook.configs["flat/recommended"]];
